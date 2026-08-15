a118924f6dd26ba13976dc169800086bc3a43961
Muhammad Imran <imran271828@gmail.com>
2026-08-15 18:02:56 +0200

    Add media monitoring keyword

--- profile.py
+++ profile.py
@@ -19,6 +19,7 @@ JOURNALISM_MEDIA = [
     "political communication", "media communication",
     "public interest media", "the press", "broadcasting",
     "journalistic", "journalism studies",
+    "media monitoring",
     "news recommendation", "news algorithms", "news analysis",
     "computational communication",
 ]
